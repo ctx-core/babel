d2b4bc5863b89aa646ec56057e3bdf6b3a977717
Brian Takita <brian.takita@gmail.com>
2020-09-17 15:30:10 -0700

    typescript updates

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
-import '@ctx-core/env/env'
-import { _version__package } from '@ctx-core/package'
+import '@ctx-core/env'
+import { _package_version } from '@ctx-core/package'
 export const BABEL__POLYFILL__URL =
 	`https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/${_version__babel_polyfill()}/polyfill.min.js`
 export function _version__babel_polyfill() {
-	return _version__package('babel-polyfill')
+	return _package_version('babel-polyfill')
 }
